85ad623e167492cba77c7e5d07be22f888276f7d
Annzstbl <345155252@qq.com>
2019-03-20 22:03:09 +0800

    read car

--- dispatch.cpp
+++ dispatch.cpp
@@ -1,13 +1,19 @@
 #include "dispatch.h"
 #include "fstream"
 using namespace std;
-int Read_car_(string car_path)
+
+int Dispatch::Read_car_()
 {
-	ifstream file_car(car_path,ios::in);
-	string dat;
+	ifstream file_car(carPath_,ios::in);
+	string data_car;
+	int id, starting_cross, ending_cross, max_speed, estimated_time;
 	if (!file_car)
 		return 0;
-	//while()
-	getline(file_car,dat);
+	while (getline(file_car, data_car))
+	{
+		if (data_car[1] == '#')
+			break;
+		//sprintf(data_car,"")
+	}
 
 }
--- dispatch.h
+++ dispatch.h
@@ -10,18 +10,28 @@ using namespace std;
 class Dispatch
 {
 public:
-	Dispatch();
+	Dispatch(string carPath, string roadPath, string crossPath, string answerPath)
+	{
+		carPath_ = carPath;
+		roadPath_ = roadPath;
+		crossPath_ = crossPath;
+		answerPath_ = answerPath;
+	}
 	void Initiate_();							//read input files
-	int Read_car_(string car_path);
+	int Read_car_();
 	int Read_road_();
 	int Read_cross_();
 	void Boost_one_();							//time = time + 1
 
 
 private:
-	vector<Cross*> d_crosses_;		//dispatch_crosses_;
-	vector<Car*> d_cars_;			//all cars
-	vector<Road*> d_roads_;
+	string carPath_;
+	string roadPath_;
+	string crossPath_;
+	string answerPath_;
+	vector<Cross> d_crosses_;		//dispatch_crosses_;
+	vector<Car> d_cars_;			//all cars
+	vector<Road> d_roads_;
 
 	std::vector<int> d_cars_driving_;			//cars which are driving in road
 	std::vector<std::vector<int>> d_cars_route_;//cars route
